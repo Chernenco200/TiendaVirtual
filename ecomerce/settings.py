d4f3700b63422404ce0cec41c579abac7571901a
Ruidias Román <jruidias@mef.gob.pe>
2026-04-23 23:35:31 -0500

    final

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'django_userforeignkey',
     'rest_framework',
 
+    'core', 
+
  
 
     
--- ecomerce/settings3.py
+++ ecomerce/settings3.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'store.apps.StoreConfig',
     'django_filters' ,
+    "core", 
     
 
 ]
