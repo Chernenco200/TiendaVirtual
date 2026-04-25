ad6de2595e69dfcab609018470c9e867d0a57487
Ruidias Román <jruidias@mef.gob.pe>
2026-04-25 16:30:41 -0500

    retornar

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -92,13 +92,21 @@ WSGI_APPLICATION = 'ecomerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("HEROKU_POSTGRESQL_IVORY_URL") or os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+# ========= DATABASE (Heroku Postgres) =========
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 
