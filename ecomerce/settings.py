e3ce4861ace8f5067f3b02fd87a8c6b504d098a0
Ruidias Román <jruidias@mef.gob.pe>
2026-04-25 23:15:40 -0500

    CORREGIRSETTING5

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -201,4 +201,17 @@ STORAGES = {
 }
 
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
+
 
