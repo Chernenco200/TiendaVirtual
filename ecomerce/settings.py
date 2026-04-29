6ee2723e679e340286affd899184bf946a2428e9
Ruidias Román <jruidias@mef.gob.pe>
2026-04-29 01:35:49 -0500

    static4

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -173,3 +173,18 @@ STORAGES = {
     },
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
